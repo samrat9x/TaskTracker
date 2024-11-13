749d204fa13e94a99a98101576d6c4545eb07093
samrat9x <primeminister9xm@gmail.com>
2024-11-13 22:47:54 +0600

    asdf

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,23 @@
-const CACHE_NAME = "task-time-tracker-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+const cacheName = "complement-cache-v1";
+const assetsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192x192.png", // Add your icon files here
+  "icon-512x512.png",
 ];
 
-// Install Service Worker and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
     })
   );
 });
 
-// Fetch and serve cached files
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
-  );
-});
-
-// Update Service Worker and remove old cache
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
     })
   );
 });
