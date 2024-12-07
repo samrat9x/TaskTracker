8391c1f8e8b82f81618314edfe236097d0fdfb59
samrat9x <primeminister9xm@gmail.com>
2024-12-07 21:36:47 +0600

    fds

--- service-worker.js
+++ service-worker.js
@@ -42,3 +42,32 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+/* @@Previous Version
+
+const cacheName = "complement-cache-v1";
+const assetsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192x192.png", // Add your icon files here
+  "icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+*/
