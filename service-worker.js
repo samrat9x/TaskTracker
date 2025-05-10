7a4d624ae9ff9400b6f525431650208ed78545e7
samrat9x <primeminister9xm@gmail.com>
2025-05-10 18:49:33 +0600

    update cache version to v3 and enhance task list functionality with sortable feature

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "complement-cache-v2"; // Updated cache version
+const cacheName = "complement-cache-v3"; // Updated cache version
 const assetsToCache = [
   "index.html",
   "manifest.json",
