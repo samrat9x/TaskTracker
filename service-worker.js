6ac671446604b3ac5959a0b7b1ff29f7d98058c5
samrat9x <primeminister9xm@gmail.com>
2025-05-14 17:17:08 +0600

    fix: update cache version to complement-cache-v7 and enhance task item event handling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "complement-cache-v1"; // Updated cache version
+const cacheName = "complement-cache-v7"; // Updated cache version
 const assetsToCache = [
   "index.html",
   "style.css",
