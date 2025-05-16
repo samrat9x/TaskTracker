fa2f03438325358850d9b0cfae803e1134cadfc6
samrat9x <primeminister9xm@gmail.com>
2025-05-16 18:05:19 +0600

    fix: move edit popup close event listener to avoid duplicate bindings

--- task.js
+++ task.js
@@ -93,10 +93,6 @@ function editTask(index) {
   editPopup.style.display = "flex"; // Show the edit popup
   editTaskNameInput.value = tasks[activeTab][index].name; // Set the input field to the current task name
   indexPreserve = index; // Preserve the index of the task being edited
-  editClosePopup.addEventListener("click", () => {
-    editPopup.style.display = "none"; // Hide the edit popup when the close button is clicked
-    editTaskNameInput.value = ""; // Clear the input field
-  }); // Close the edit popup when the close button is clicked
 }
 
 editSave.addEventListener("click", () => {
@@ -109,7 +105,10 @@ editSave.addEventListener("click", () => {
     editTaskNameInput.value = ""; // Clear the input field
   }
 }); // Save the edited task when the save button is clicked
-
+editClosePopup.addEventListener("click", () => {
+  editPopup.style.display = "none"; // Hide the edit popup when the close button is clicked
+  editTaskNameInput.value = ""; // Clear the input field
+}); // Close the edit popup when the close button is clicked
 //--------------------------------------------------------------------------------
 
 function deleteTask(index) {
