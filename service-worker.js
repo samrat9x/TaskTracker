b3a36caff2f76c8137ea2a040be656e9d549eb9d
samrat9x <primeminister9xm@gmail.com>
2025-05-18 00:13:58 +0600

    fix: update cache version to v17

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "complement-cache-v16"; // Updated cache version
+const cacheName = "complement-cache-v17"; // Updated cache version
 const assetsToCache = [
   "index.html",
   "style.css",
