9543af791e4e0d01dc3cc963e6969fb264c2644a
samrat9x <primeminister9xm@gmail.com>
2025-05-18 22:44:44 +0600

    fix: update cache version to v26 and remove background-size property from main styles

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "complement-cache-v25"; // Updated cache version
+const cacheName = "complement-cache-v26"; // Updated cache version
 const assetsToCache = [
   "index.html",
   "style.css",
